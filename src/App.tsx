7ca8440824132a90d4660e8aa3afb72d9a854cf9
Jorge Venegas <jorgepvenegas@protonmail.com>
2024-03-23 13:22:01 -0700

    add tailwind, styled components and daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { createEffect, createSignal, type Component } from 'solid-js';
-import styles from './App.module.css';
 import { PlatePair, availablePlates, calculatePlates } from './utils/calculators';
+import { styled } from "solid-styled-components";
 
+const PlateSelector = styled("div")``;
+const Header = styled("h1")`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
+
+const ResultHeader = styled("h2")`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
 
 
 const App: Component = () => {
@@ -36,45 +46,76 @@ const App: Component = () => {
   }
 
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <div>
-          {selectedPlates().map(({ enabled, weight }, index) => (
-            <div>
-              <input type="checkbox" name={`${weight}-plate`} checked={enabled} onChange={() => handlePlateCheckbox(index)} />
-              <label for={`${weight}-plate`}>{`${weight} plate`}</label>
-            </div>
-          ))}
+    <div class="container mx-auto w-4/12 min-w-96 flex flex-col gap-3 min-h-screen justify-center">
+      <Header>Barbell load calculator</Header>
+      <div class="collapse collapse-arrow  bg-base-200">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium">
+          Customize plates
         </div>
-        <p class={styles.emojiThing}>🏋️</p>
-        <select onChange={(e) => {
+        <div class="collapse-content">
+          <PlateSelector class="grid gap-2 grid-cols-4">
+            {selectedPlates().map(({ enabled, weight }, index) => (
+              <button class="btn btn-success btn-block btn-lg h-24 w-24flex flex-col justify-center items-center no-animation" onClick={() => handlePlateCheckbox(index)}>
+                <label for={`${weight}-plate`}>{`${weight} lb`}</label>
+                <input class="checkbox" type="checkbox" name={`${weight}-plate`} checked={enabled} />
+              </button>
+            ))}
+          </PlateSelector>
+        </div>
+      </div>
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">Pick a bar</span>
+        </div>
+        <select class="select select-bordered w-full" onChange={(e) => {
           setBarWeight(Number(e.target.value))
         }}>
           <option value={35}>35lb</option>
           <option value={45} selected>45lb</option>
         </select>
-        <input class={styles.bigInput} value={weight()} type="number" min={45} max={9000} onKeyUp={onKeyUp} />
-        {weight() <= barWeight() && (
-          <p>Need <i>at least </i>{barWeight()} lb 🏋️</p>
-        )}
-        {weight() === 9000 ? (
-          <p>IT'S OVER 9000!</p>
-        ) : weight() >= 1000 ? (
-          <p>WOAH DUDE!</p>
-        ) : weight() >= 300 ? (
-          <p>So strong...😏</p>
-        ) : null}
-        {plates() && (
-          <div>
-            <small>Considering a {barWeight()}(?) lb bar and<br /> the  plates available <br /> you need</small>
-            <ul class={styles.ul}>
+      </label>
+
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">Target weight</span>
+        </div>
+        <input class="input input-bordered w-full" value={weight()} type="number" min={45} max={9000} onKeyUp={onKeyUp} />
+      </label>
+
+
+      {weight() <= barWeight() && (
+        <div role="alert" class="alert alert-info">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>Need <i>at least </i>{barWeight()} lb</span>
+        </div>
+      )}
+      {weight() === 9000 ? (
+        <div role="alert" class="alert alert-info">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>It's over 9000!</span>
+        </div>
+      ) : weight() >= 1000 ? (
+        <div role="alert" class="alert alert-info">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>WOAH DUDE</span>
+        </div>
+      ) : weight() >= 300 ? (
+        <div role="alert" class="alert alert-info">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>So strong...😏</span>
+        </div>
+      ) : null}
+      {plates() && (
+        <div class="p-4 bg-base-200 rounded-box">
+            <ResultHeader class="pb-3">🏋️ You need</ResultHeader>
+            <ul class="list-disc px-4">
               {plates()?.map(({ count, weight }) => (
                 <li>{count} plate(s) of {weight}lb per side</li>
               ))}
             </ul>
-          </div>
-        )}
-      </header>
+        </div>
+      )}
     </div >
   );
 };
