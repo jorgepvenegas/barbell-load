be17b09f2d25017b7d1e1d6174b52d088fc31b7d
Jorge Venegas <jorgepvenegas@protonmail.com>
2024-03-22 18:54:51 -0700

    random stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { createEffect, createSignal, type Component } from 'solid-js';
 import styles from './App.module.css';
-import { availablePlates, calculatePlates } from './utils/calculators';
+import { PlatePair, availablePlates, calculatePlates } from './utils/calculators';
+
+
 
 const App: Component = () => {
 
   const [weight, setWeight] = createSignal(95);
-  const [plates, setPlates] = createSignal<Array<string>>();
+  const [plates, setPlates] = createSignal<Array<PlatePair>>();
+  const [barWeight, setBarWeight] = createSignal<number>(45);
 
   createEffect(() => {
-    const { plates } = calculatePlates(weight());
+    const plates = calculatePlates({
+      targetWeight: weight(),
+      barWeight: barWeight()
+    });
     setPlates(plates)
   });
 
   const onKeyUp = (e) => {
     const newWeight = Number(e.target.value) > 9000 ? 9000 : Number(e.target.value);
-    console.log(newWeight);
-    
     setWeight(newWeight);
   }
 
@@ -23,23 +27,29 @@ const App: Component = () => {
     <div class={styles.App}>
       <header class={styles.header}>
         <p class={styles.emojiThing}>🏋️</p>
-        <input class={styles.bigInput} value={weight()} type="number" onKeyUp={onKeyUp} />
-        {weight() <= 45 && (
-          <p>Need <i>at least </i>45 lb 🏋️</p>
+        <select onChange={(e) => {
+          setBarWeight(Number(e.target.value))
+        }}>
+          <option value={35}>35lb</option>
+          <option value={45} selected>45lb</option>
+        </select>
+        <input class={styles.bigInput} value={weight()} type="number" min={45} max={9000} onKeyUp={onKeyUp} />
+        {weight() <= barWeight() && (
+          <p>Need <i>at least </i>{barWeight()} lb 🏋️</p>
         )}
         {weight() === 9000 ? (
           <p>IT'S OVER 9000!</p>
         ) : weight() >= 1000 ? (
           <p>WOAH DUDE!</p>
-        ) : weight() >= 400 ? (
+        ) : weight() >= 300 ? (
           <p>So strong...😏</p>
         ) : null}
         {plates() && (
           <div>
-            <small>Considering a 45(?) lb bar and<br /> {availablePlates.join(', ')} lb plates available <br /> you need</small>
+            <small>Considering a {barWeight()}(?) lb bar and<br /> {availablePlates.join(', ')} lb plates available <br /> you need</small>
             <ul class={styles.ul}>
-              {plates()?.map(p => (
-                <li>{p}  per side</li>
+              {plates()?.map(({ count, weight }) => (
+                <li>{count} plate(s) of {weight}lb per side</li>
               ))}
             </ul>
           </div>
--- src/utils/calculators.ts
+++ src/utils/calculators.ts
@@ -1,18 +1,28 @@
 type PlateAggregator = {
-    totalWeight: number;
+    targetWeight: number;
     remainingWeight: number;
-    plates: Map<number, number>; // weight, count
+    plates: Map<PlateWeight, number>; // weight, count
 }
 
-export const availablePlates = [55, 45, 35, 25, 15, 10, 5, 2.5, 1, 0.5];
-export const calculatePlates = (totalWeight: number) => {
-    const barWeight = 45;
+export type BarWeight = 35 | 45;
+export type PlateWeight = .25 | .5 | 1 | 2.5 | 5 | 10 | 15 | 25 | 35 | 45 | 55;
+
+export type Plates = Array<PlateWeight>;
+
+export const availablePlates: Plates = [55, 45, 35, 25, 15, 10, 5, 2.5, 1, 0.5];
+
+export type PlatePair = {
+    count: number;
+    weight: PlateWeight
+};
+
+export const calculatePlates = ({ targetWeight, barWeight }: { targetWeight: number, barWeight: number }): Array<PlatePair> => {
 
     const { plates } = availablePlates.reduce((acc, next) => {
         const plate = next;
         function addPlates() {
             if (acc.remainingWeight >= plate * 2) {
-                if(acc.plates.get(plate)) {
+                if (acc.plates.get(plate)) {
                     acc.plates.set(plate, acc.plates.get(plate)! + 1)
                 }
                 else {
@@ -24,14 +34,17 @@ export const calculatePlates = (totalWeight: number) => {
             return acc;
         }
         return addPlates();
-    }, { totalWeight, remainingWeight: totalWeight - barWeight, plates: new Map<number, number>() } as PlateAggregator)
+    }, { targetWeight, remainingWeight: targetWeight - barWeight, plates: new Map<PlateWeight, number>() } as PlateAggregator)
 
     // Simplify the output as array of string so we use them in a unordered list
-    let output = [];
+    let output: Array<PlatePair> = [];
 
-    for(const[weight, count] of plates.entries()) {
-        output.push(`${count} plate(s) of ${weight} lb`);
+    for (const [weight, count] of plates.entries()) {
+        output.push({
+            count,
+            weight
+        });
     }
-    
-    return { plates: output }
+
+    return output;
 }
