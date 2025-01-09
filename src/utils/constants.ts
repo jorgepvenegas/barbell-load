49a926e6637c5245138d0745d37b34d175d095f9
Jorge Venegas <jorgepvenegas@protonmail.com>
2025-01-08 23:36:52 -0800

    Enhance PlateResults component

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const STORAGE_KEY = "selectedPlates";
-export const MIN_PERCENTAGE = 70;
+export const MIN_PERCENTAGE = 40;
 export const MAX_PERCENTAGE = 100;
 export const MAX_WEIGHT = 1500;
 export const PERCENTAGE_STEP = 5;
